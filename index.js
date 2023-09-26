36d11ae94b32f7d3a831f469d2d8e6ad31d99590
Antonio Soler <asolergo@gmail.com>
2023-09-26 13:36:28 +0200

    test commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 4040
+const express = require("express");
+const app = express();
+const port = 4041;
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://asolermaria:cloa1997@cluster0.bmbqxd3.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://asolermaria:cloa1997@cluster0.bmbqxd3.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -11,7 +12,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -20,7 +21,9 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
@@ -28,18 +31,13 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/detalleArticulo/:id_articulo", (req, res) => {
+  // Select a la bd, a la tabla de articulos where id_articulo
 
-
-app.get('/detalleArticulo/:id_articulo', (req, res) => {
-
- // Select a la bd, a la tabla de articulos where id_articulo
- 
- //enviarla de vuelta con el contenido de la salida en formato json
-  res.send('Hello World tonto! {_id}')
- 
-})
+  //enviarla de vuelta con el contenido de la salida en formato json
+  res.send("Hello World tonto! {_id}");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});
