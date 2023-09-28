8de6a5bb43eef17d66b685854669e0ffa3d3b9d5
Antonio Soler <asolermaria@gmail.com>
2023-09-28 13:56:57 +0200

    2809 v1

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ async function run() {
 }
 run().catch(console.dir);
 
-/*******DEFINICION APIS *********/
-//Definición API detalleAritculo
+/****** INICIO DEFINICION APIS *******/
+
 app.get("/detalleArticulo", async (req, res) => {
   // Recogida variables introducidas en la llamada al API
   var idArticulo = req.query.idArticulo;
@@ -53,7 +53,97 @@ app.get("/detalleArticulo", async (req, res) => {
   else res.send(result).status(200);
 });
 
-/******* FIN DEFINICION DE APIS */
+app.get("/consultaUsuario", async (req, res) => {
+  // Recogida variables introducidas en la llamada al API
+  var idUsuario = req.query.idUsuario;
+
+  //Conexión a la colección
+  let collection = await db.collection("usuarios");
+
+  //Construcción de la query
+  let query = { id_usuario : idUsuario };
+
+  //Ejecución
+  let result = await collection.findOne(query);
+
+  //TODO: Salida: nombre_usuario, correo_usuario, contraseña_usuario.
+
+  // Devolución de resultados
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
+app.get("/detallePedido", async (req, res) => {
+  // Recogida variables introducidas en la llamada al API
+  var idPedido = req.query.idPedido;
+
+  //Conexión a la colección
+  let collection = await db.collection("pedidos");
+
+  //Construcción de la query
+  let query = { id_pedido : idPedido };
+
+  //Ejecución
+  let result = await collection.findOne(query);
+
+  // Devolución de resultados
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
+app.get("/listaCarrito", async (req, res) => {
+  // Recogida variables introducidas en la llamada al API
+  var idUsuario = req.query.idUsuario;
+
+  //Conexión a la colección
+  let collection = await db.collection("carrito_compra");
+
+  //Construcción de la query
+  let query = { id_usuario : idUsuario };
+
+  //Ejecución
+  let result = await collection.findOne(query);
+
+  // Devolución de resultados
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
+/*Descripción: Este API devolverá la lista de artículos de la tienda que cumplan los criterios que se faciliten en la entrada.
+- Entrada: Categoria, , modelo, , , precio mínimo, precio máximo. Estos datos de entrada serán opcionales y excepto precio mínimo y precio máximo, el resto permitirá introducir varios valores para cada campo.
+- Salida: id_articulo, foto_articulo, nombre_articulo y precio_articulo.*/
+app.get("/listaArticulos", async (req, res) => {
+  // Recogida variables introducidas en la llamada al API
+  var marcaArticulo = req.query.marcaArticulo;
+  var modeloArticulo = req.query.modeloArticuloArticulo;
+  var almacenamientoArticulo = req.query.almacenamientoArticulo;
+  var colorArticulo = req.query.colorArticulo;
+  var precioMinimo = parseFloat(req.query.precioMinimo);
+  
+
+  //Conexión a la colección
+  let collection = await db.collection("articulos");
+
+  //Construcción de la query
+  let query = { marca_articulo : marcaArticulo,
+                modelo_articulo : modeloArticulo,
+                almacenamiento_articulo : almacenamientoArticulo,
+                color_articulo : colorArticulo,
+                precio_articulo: { $gte: precioMinimo }};
+                //TODO precioMaximo
+
+  //Ejecución
+  let result= await collection.find(query).toArray();
+
+  
+
+  // Devolución de resultados
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
+/****** FIN DEFINICION DE APIS ****/
+
 //Levantar el servidor
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
